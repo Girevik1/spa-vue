9f2a1112f312219ae95566411f447ebc9b20da92
girevik1 <girevik1988@gmail.com>
2021-05-05 00:03:18 +0300

    main page

--- src/App.vue
+++ src/App.vue
@@ -1,67 +1,293 @@
 <template>
-  <div id="app">
-    <v-card class="overflow-hidden">
-    <v-app-bar
+  <v-app>
+    <v-navigation-drawer
+      class="mx-auto"
+      v-if="drawer"
+      app
       absolute
-      color="#43a047"
-      dark
-      shrink-on-scroll
-      prominent
-      src="https://picsum.photos/1920/1080?random"
-      fade-img-on-scroll
-      scroll-target="#scrolling-techniques-5"
-      scroll-threshold="500"
+      style="background-color: #0f131e"
+      width="auto"
+      height="auto"
+      permanent
     >
-      <template v-slot:img="{ props }">
-        <v-img
-          v-bind="props"
-          gradient="to top right, rgba(55,236,186,.7), rgba(25,32,72,.7)"
-        ></v-img>
-      </template>
+      <v-list>
+        <!-- <center><b>Пиши:</b></center> -->
+        <v-list-item v-for="(item, i) in items" :key="i" link>
+          <v-list-item-icon>
+            <v-icon style="color:white">{{ item.icon }}</v-icon>
+          </v-list-item-icon>
 
-      <v-app-bar-nav-icon></v-app-bar-nav-icon>
+          <v-list-item-content>
+            <v-list-item-title style="color:white">{{
+              item.title
+            }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
 
-      <v-app-bar-title>Title</v-app-bar-title>
+    <v-app-bar style="background-color: #0f131e" dense dark app>
+      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+
+      <v-toolbar-title
+        ><v-img
+          :src="require('@/assets/logo_frendex.png')"
+          max-height="50px"
+          max-width="80px"
+        ></v-img
+      ></v-toolbar-title>
 
       <v-spacer></v-spacer>
-
-      <v-btn icon>
-        <v-icon>mdi-magnify</v-icon>
-      </v-btn>
-
-      <v-btn icon>
-        <v-icon>mdi-heart</v-icon>
-      </v-btn>
-
-      <v-btn icon>
-        <v-icon>mdi-dots-vertical</v-icon>
-      </v-btn>
     </v-app-bar>
+
     <v-sheet
-      id="scrolling-techniques-5"
+      id="scrolling-techniques-3"
       class="overflow-y-auto"
-      max-height="600"
+      style="background-color: #15172e"
     >
-      <v-container style="height: 1500px;"></v-container>
+      <v-container style="height: auto;margin-top: 50px;">
+        <v-card-text style="color:white;font-size: 15px;"
+          ><p style="text-align:center;font-size: 39px;line-height: 1.1;">
+            ФРЕНДЕКС, МОЖНО ЛИ ЗАРАБОТАТЬ?
+          </p>
+        </v-card-text>
+
+        <v-row dense>
+          <v-col
+            cols="3"
+            style="min-width: 100px; max-width: 100%;"
+            class="flex-grow-1 flex-shrink-0"
+          >
+            <v-card
+              class="mx-auto"
+              max-width="344"
+              style="background-color: #151821;color:white;box-shadow: 0 0 10px rgb(0 0 0);"
+            >
+              <v-img
+                :src="require('@/assets/ainur.png')"
+                height="400px"
+              ></v-img>
+
+              <v-card-title>
+                Меня зовут Айнур
+              </v-card-title>
+
+              <v-card-subtitle
+                style="color:white;font-style: italic;font-size: 15px;line-height: 1.6;"
+              >
+                Если вам интересен пассивный доход, инструмент, который будет
+                приносить деньги, даже когда вы сидите дома, то вам думаю, будет
+                интересен проект ФРЕНДЕКС
+              </v-card-subtitle>
+            </v-card>
+          </v-col>
+          <v-col
+            cols="8"
+            style="min-width: 100px; max-width: 100%;margin-left:20px;margin-right:20px;"
+            class="flex-grow-1 flex-shrink-1"
+          >
+            <v-card
+              class="mx-auto"
+              style="background-color: #151821;color:white;box-shadow: 0 0 10px rgb(0 0 0);"
+            >
+              <v-card-title style="font-size: 19px;">
+                Frendex - закрытый инвестиционный клуб, благодаря которому
+                каждый желающий<br />может начать зарабатывать от 0,7% до 2%
+                ежедневно.
+              </v-card-title>
+              <hr />
+              <v-card-subtitle
+                style="line-height: 2;font-size: 16.4px;color:white;"
+                >Из множества компаний с высокой доходностью я выбрал именно эту
+                по следующим причинам:<br />
+
+                - Компания довольно свежая. Дата старта август 2020г.<br />
+
+                - Нет негативных отзывов. Все выплаты вкладчики получают вовремя
+                и благополучно.<br />
+
+                - Большая доходность. От 30% в месяц, от вложенной суммы. (То
+                есть, вложенные средства оправдываются уже за 3 месяца, затем
+                выходишь на чистую прибыль).<br />
+
+                - Нет комиссии за вывод средств. К примеру, в других подобных
+                компаниях везде существует комиссия за вывод.<br />
+
+                - Можно внести любую сумму, начиная от 100 долларов.<br />
+
+                - И лично для меня, существенным плюсом компании является
+                возможность реинвестирования средств. Можно создать
+                капитализацию средств своего счета Френдекс. Можно добавлять
+                полученные дивиденды к своему депозиту и уже будет работать
+                общая сумма (изначальный депозит-плюс полученные дивиденды).<br />
+
+                - Считаю важным плюсом открытость основателя Френдекс -
+                <b>Руслана Пичугина</b>. Он ездит по городам и рассказывает про
+                свое детище.
+              </v-card-subtitle>
+            </v-card>
+          </v-col>
+        </v-row>
+        <v-row dense>
+          <v-col
+            cols="3"
+            style="min-width: 100px; max-width: 100%;margin-top: 30px;"
+            class="flex-grow-1 flex-shrink-0"
+          >
+            <v-card
+              class="mx-auto"
+              style="background-color: #151821;color:white;box-shadow: 0 0 10px rgb(0 0 0);"
+            >
+              <v-card-title>
+                Деньги инвесторов<br />трейдеры компании<br />Френдекс
+                вкладывают<br />в такие направления как:
+              </v-card-title>
+              <hr />
+              <v-card-subtitle
+                style="line-height: 1.7;font-size: 15px;color:white"
+              >
+                - Выкуп с торгов недвижимости банкротов по выгодной цене<br />-
+                Эффективные сделки на Московской бирже и валютном рынке<br />-
+                Строительный бизнес<br />- Развитие стартапов<br /><br />Доходность
+                компании составляет 3-5 % в день.<br />Из полученных денег почти
+                треть идет на выплату партнерам, то есть НАМ!<br />Остальное
+                тратят на хозяйственные нужды, зарплату сотрудникам компании и
+                формирование резерва.
+              </v-card-subtitle>
+            </v-card>
+          </v-col>
+          <v-col
+            cols="9"
+            style="min-width: 100px; max-width: 100%;margin-top: 30px;"
+            class="flex-grow-1 flex-shrink-0"
+          >
+            <v-carousel
+              cycle
+              height="auto"
+              hide-delimiter-background
+              show-arrows-on-hover
+            >
+              <v-carousel-item v-for="(slide, i) in slides" :key="i">
+                <center>
+                  <img
+                    :lazy-src="slide.src"
+                    :src="slide.src"
+                    style="width:auto;height:610px;"
+                    :alt="slide.src"
+                  />
+                </center>
+              </v-carousel-item>
+            </v-carousel>
+          </v-col>
+        </v-row>
+        <v-row dense>
+          <v-col
+            cols="12"
+            style="min-width: 100px; max-width: 100%;"
+            class="flex-grow-1 flex-shrink-0"
+          >
+            <v-card
+              class="mx-auto"
+              style="background-color: #151821;box-shadow: 0 0 10px rgb(0 0 0);margin-top: 30px;"
+            >
+              <v-card-subtitle
+                style="color:white;
+                font-size:15px;
+                line-height:1.7;"
+              >
+                Масштабность и успех закрытого инвестиционного клуба Frendex
+                видна в открытии офисов и получении автомобилей. Также
+                проводятся конференции, планируют открыть офис в Москве и многих
+                городах бывшего СНГ.<br />Реальные отзывы о Френдекс появляются
+                на Youtube и в общем Telegram чате. Люди вкладывают большие
+                деньги. Веской причиной добавления проекта в портфель стал
+                переход лидеров из разных инвестиционных программ.<br />Просто
+                так происходить такие процессы не могут. Это значит, что в
+                будущем будут строить мощные команды, а главное, что в интернете
+                информации еще практически о клубе нет. Если будет ребрендинг,
+                тогда только начнется основная раскачка.
+              </v-card-subtitle>
+            </v-card>
+          </v-col>
+        </v-row>
+
+        <v-row dense>
+          <v-col
+            cols="6"
+            style="min-width: 100px; max-width: 100%;margin-top: 30px;"
+            class="flex-grow-1 flex-shrink-0"
+          >
+            <v-card
+              class="mx-auto"
+              style="background-color: #151821;color:white;box-shadow: 0 0 10px rgb(0 0 0);"
+            >
+              <v-img :src="require('@/assets/auto.png')" height="400px"></v-img>
+            </v-card>
+          </v-col>
+          <v-col
+            cols="6"
+            style="min-width: 100px; max-width: 100%;margin-top: 30px;"
+            class="flex-grow-1 flex-shrink-0"
+          >
+            <v-card
+              class="mx-auto"
+              style="background-color: #151821;color:white;box-shadow: 0 0 10px rgb(0 0 0);"
+            >
+              <v-card-title>
+                Auto за часть от ее стоимости
+              </v-card-title>
+              <hr />
+
+              <v-card-title style="line-height: 2;font-size: 16px;color:white"
+                >В компании так же есть возможность приобрести поддержанное или
+                новое авто за часть от ее стоимости.<br /><br />⦁ Взнос 30% —
+                130 дней ожидания — 2% вознаграждение<br /><br />⦁ Взнос 35% —
+                120 дней ожидания — 3% вознаграждение<br /><br />⦁ Взнос 40% —
+                110 дней ожидания — 4% вознаграждение
+              </v-card-title>
+            </v-card>
+          </v-col>
+        </v-row>
+      </v-container>
     </v-sheet>
-  </v-card>
-    <router-view/>
-  </div>
+  </v-app>
 </template>
 
 <script>
 export default {
-  name: 'App'
-}
+  name: "App",
+  data() {
+    return {
+      items: [
+        { title: "Telegram", icon: "mdi-view-dashboard" },
+        { title: "WhatsApp", icon: "mdi-account-box" },
+        { title: "Vk", icon: "mdi-gavel" }
+      ],
+      drawer: false,
+      // tab: null,
+      // itemsTab: ["Главная страница", "О компании"],
+      slides: [
+        { src: require("@/assets/slide.png") },
+        { src: require("@/assets/slide1.png") },
+        { src: require("@/assets/slide2.png") }
+      ]
+    };
+  }
+};
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  color: #15172e;
+}
+.navigation {
+  color: #15172e;
+}
+.bar {
+  color: #15172e;
+}
+img {
+  box-shadow: 0 0 10px rgb(0 0 0);
+  border-radius: 10px;
 }
 </style>
